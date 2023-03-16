a71695dbd5d8d2255094c673f3d58866a7fe3982
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-16 05:24:47 -0700

    committing 4-puts.c (that prints a text using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char my_list[100]={"\"Programming is like building a multilingual puzzle"};
+    puts(my_list);
+    return (0);
+}
