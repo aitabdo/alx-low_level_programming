8a64ee6bb8aaa2bb7faea41b8a73e19cfc042b1e
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-17 11:28:39 -0700

    committing files

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,7 +7,7 @@
 * main - print if the number is postive, zero, or negative
 *
 * Description: using the main function
-* this program prints "prints the lowercase alphabet in reverse,followed by newline
+* this program prints "the lowercase alphabet in reverse,followed by newline
 * Return: 0
 */
 int main(void)
@@ -19,7 +19,7 @@ char L[27] = {"abcdefghijklmnopqrstuvwxyz"};
 
 for (i = 0; i < 26; i++)
 {
-	putchar(L[(strlen(L)-1)-i]);
+	putchar(L[(strlen(L) -1 ) - i]);
 }
 printf("\n");
 return (0);
