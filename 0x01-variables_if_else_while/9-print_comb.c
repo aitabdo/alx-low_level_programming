7404dc927156a4d24100ddd7d888ff0e8630974a
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-17 14:14:28 -0700

    committing files

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include <time.h>
+
+/**
+* main - print if the number is postive, zero, or negative
+*
+* Description: using the main function
+* this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8,9
+* Return: 0
+*/
+int main(void)
+{
+int i;
+
+/* your code goes there */
+int l[11] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+for (i = 0 ; i < 9 ; i++)
+{
+	putchar(l[i] + '0');
+	putchar(',');
+	putchar(' ');
+}
+putchar(a[9] + '0');
+printf("\n");
+return (0);
+}
