95873ff366e65914c68c9c1a189c18fc8f79745f
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-17 09:25:44 -0700

    committing files

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include <time.h>
+
+/**
+* main - print if the number is postive, zero, or negative
+*
+* Description: using the main function
+* this program prints "prints the alphabet in lowercase, followed by a new line
+* Return: 0
+*/
+int main(void)
+{
+char my_list[100]={"abcdefghijklmnopqrstuvwxyz"};
+putchar(my_list);
+}
