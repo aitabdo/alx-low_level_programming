8924f051112dcf6715eec2d0435963f7045578c0
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-17 08:28:46 -0700

    committing files

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -31,7 +31,7 @@ int main(void)
         {
 		printf("%d and is 0", x);
         }
-	if (x > 6 && x != 0)
+	if (x < 6 && x != 0)
         {
 		printf("%d and is less than 6 and not 0", x);
         }
