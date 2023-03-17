74d7a37a077fc3af6d93f2e796a5cc80a6da9b5c
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-17 11:43:10 -0700

    committing files

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include <time.h>
+
+/**
+* main - print if the number is postive, zero, or negative
+*
+* Description: using the main function
+* this program prints "123456789
+* Return: 0
+*/
+int main(void)
+{
+int i;
+
+/* your code goes there */
+char l[11] = {"0123456789"};
+char n[7] = {"abcdef"};
+for (i = 0;i < 10; i++)
+{
+	putchar(l[i]);
+}
+for (i = 0;i < 6; i++)
+{
+        putchar(n[i]);
+}
+printf("\n");
+return (0);
+}
