19665f626d25ffd237065f08834dbb9cb65ee3a2
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-17 11:24:25 -0700

    committing files

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include <time.h>
+
+/**
+* main - print if the number is postive, zero, or negative
+*
+* Description: using the main function
+* this program prints "123456789
+* Return: 0
+*/
+int main(void)
+{
+/* your code goes there */
+printf("0123456789\n");
+return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include <time.h>
+#include<string.h>
+
+/**
+* main - print if the number is postive, zero, or negative
+*
+* Description: using the main function
+* this program prints "prints the lowercase alphabet in reverse,followed by newline
+* Return: 0
+*/
+int main(void)
+{
+int i;
+
+/* your code goes there */
+char L[27] = {"abcdefghijklmnopqrstuvwxyz"};
+
+for (i = 0; i < 26; i++)
+{
+	putchar(L[(strlen(L)-1)-i]);
+}
+printf("\n");
+return (0);
+}
