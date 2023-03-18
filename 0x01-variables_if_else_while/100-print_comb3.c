33a6fc7fa6ebf0de103d1939b54faea5ad311fc8
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-17 18:44:44 -0700

    committing files

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,7 +17,7 @@ int j;
 /* your code goes there */
 int a[12] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 int b[12] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-for (i = 0 ; i < 8 ; i++)
+for (i = 0 ; i < 9 ; i++)
 {
 	for (j = 0 ; j < 10 ; j++)
 	{
