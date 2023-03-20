70d8fe34cd4147b10ad5e5bea10bc1f4a43e040a
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-20 09:33:16 -0700

    committing files

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include <time.h>
+
+/**
+* main -  prints all possible different combinations of two digits
+*
+* Description: using the main function
+* this program prints "possible different combinations of two digits
+* Return: 0
+*/
+int main(void)
+{
+int i;
+
+/* your code goes there */
+int a[12] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+int b[12] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+for (i = 0 ; i < 8 ; i++)
+{
+	for (int j = 0 ; j < 10 ; j++)
+	{
+		if (i < j)
+		{
+			putchar(a[i] + '0');
+			putchar(b[j] + '0');
+			if (a[i] != 8 || b[j] != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
