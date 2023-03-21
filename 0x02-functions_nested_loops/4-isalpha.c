83759d9688f7e90a67502de298ac9c706f7623bf
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-21 16:14:31 -0700

    committing files

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * _isalpha - function to check for alphabetic haracters
+ * @c:  is the int that will use for the argument of the function
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	char a[27] = "abcdefghijklmnopqrstuvwxyz";
+	char b[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+	int d = 0;
+
+	for (i = 0 ; i < 26 ; i++)
+	{
+		if (a[i] != c)
+		{
+			d = 1;
+		}
+	}
+	for (i = 0 ; i < 26 ; i++)
+	{
+		if (b[i] != c)
+		{
+			d = 1;
+		}
+	}
+	return (d);
+}
