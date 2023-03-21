4c2d684d53ee1bae06e2294ae7b6a3828899509c
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-21 11:24:11 -0700

    committing files

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_alphabet - prints alphabets
+* Return: Always 0.
+*/
+void print_alphabet(void)
+{
+	char b[27] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0 ; i < 26 ; i++)
+	{
+		_putchar(b[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
