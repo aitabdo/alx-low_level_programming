bc1b383d218d490189c1e3e7aebfe6c3d1287968
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-21 12:33:46 -0700

    committing files

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,17 +9,12 @@
 int _islower(int c)
 {
 	char a[27] = "abcdefghijklmnopqrstuvwxyz";
-	char b[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 	int d = 0;
 
 	for (i = 0 ; i < 26 ; i++)
 	{
 		if (a[i] == c)
-		{
-			d = 1;
-		}
-		if (b[i] == c)
 		{
 			d = 1;
 		}
