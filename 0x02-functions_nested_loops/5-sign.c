6b1df6752ca8f375a03c23d636fe481badc159a4
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-22 14:59:37 -0700

    committing files

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,18 +11,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(1 + '0');
+		return (1);
 	}
-	if (n < 0)
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
 	{
 		_putchar('-');
-		_putchar(-1 + '0');
+		return (-1);
 	}
-	if (n == 0)
-	{
-		_putchar(0 + '0');
-		_putchar(0 + '0');
-
-	}
-	return (0);
 }
+
