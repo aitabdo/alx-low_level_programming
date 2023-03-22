c0533060e4f3ef86e955326f2ce82b9b89b766de
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-21 18:44:05 -0700

    committing files

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * _jack_bauer - function to print all time
+ * @void:  is the void that will use for the argument of the function
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int a[4] = {0, 1, 2};
+	int b[5] = {0, 1, 2, 3};
+	int c[7] = {0, 1, 2, 3, 4, 5};
+	int d[11] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int i, j, k, l;
+
+	for (i = 0 ; i < 3 ; i++)
+	{
+		for (j = 0 ; j < 4 ; j++)
+		{
+			for (k = 0 ; k < 6 ; k++)
+			{
+				for (l = 0 ; l < 10 ; l++)
+				{
+					_putchar(a[i] + '0');
+					_putchar(b[j] + '0');
+					_putchar(c[k] + '0');
+					_putchar(d[l] + '0');
+				}
+			}
+		}
+	}
+}
