086e76ff6f53c8406922f475d3575aaa682413a8
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-21 19:31:05 -0700

    committing files

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,25 +9,28 @@
 void jack_bauer(void)
 {
 	int a[4] = {0, 1, 2};
-	int b[5] = {0, 1, 2, 3};
+	int b[5] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int c[7] = {0, 1, 2, 3, 4, 5};
 	int d[11] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int i, j, k, l;
 
 	for (i = 0 ; i < 3 ; i++)
 	{
-		for (j = 0 ; j < 4 ; j++)
+		for (j = 0 ; j < 10 ; j++)
 		{
 			for (k = 0 ; k < 6 ; k++)
 			{
 				for (l = 0 ; l < 10 ; l++)
 				{
-					_putchar(a[i] + '0');
-					_putchar(b[j] + '0');
-					_putchar(':');
-					_putchar(c[k] + '0');
-					_putchar(d[l] + '0');
-					_putchar('\n');
+					if (a[i] != 2 && b[j] > 4)
+					{
+						_putchar(a[i] + '0');
+						_putchar(b[j] + '0');
+						_putchar(':');
+						_putchar(c[k] + '0');
+						_putchar(d[l] + '0');
+						_putchar('\n');
+					}
 				}
 			}
 		}
