c58bfae62fd3fee0c8fd470cfed906836bf670e1
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-22 14:29:00 -0700

    committing files

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,24 +4,18 @@
 /**
  * print_last_digit - function to print last digit of n
  * first if loop checks if n>0,2nd if n<0
- * @c:  is the int that will use for the argument of the function
+ * @n:  is the int that will use for the argument of the function
  * Return: 0
  */
 int print_last_digit(int n)
 {
 	int x;
 
-	if (n > 0 || n == 0)
+	x = (n % 10);
+	if (x < 0)
 	{
-		x = n % 10;
-		_putchar (x + '0');
-		return (x);
+		x = (-1 * x);
 	}
-	else
-	{
-		n = n * -1;
-		x = n % 10;
-		_putchar (x + '0');
-		return (x);
-	}
+	_putchar(x + '0');
+	return (x);
 }
