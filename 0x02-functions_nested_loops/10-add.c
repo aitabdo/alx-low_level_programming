1edf81a25340b01804d0806f8afbf4654180d8cb
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-22 15:32:21 -0700

    committing files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - fucntion that add two numbers
+ * @x: argument of this function
+ * @y: argument of this function
+ * Return: x + y
+ */
+int add(int x, int y)
+{
+	int a = x + y;
+
+	return (a);
+}
