246c2c96dc21ec1782caa3c83e6f4022e953e101
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-22 17:18:56 -0700

    committing files

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+/**
+ * main - prints sum of all multiples of 3 or 5
+ * @void: no arguments for main
+ * for loop loops over numbers bellow 1024
+ * Return :0
+ */
+int main()
+{
+	int i;
+	int summ=0;
+	int summ1=0;
+	for(i=0 ;i<1024;i++)
+	{
+		if (i % 3 == 0)
+		{
+			summ = summ + i;
+		}
+		if (i % 5 == 0)
+		{
+			summ1 = summ1 + i;
+		}
+	}
+	return (summ + summ1);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* times_table - a fucntion that prints 9 table
+* @void: no arguments 
+* Return: 0 prints 9 table as output
+*/
+void times_table(void)
+{
+
+
+}
