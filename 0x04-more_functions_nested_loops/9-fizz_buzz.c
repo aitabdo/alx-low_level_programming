93d1787d68450f2ec8efdf11594ee8a7f7084872
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-23 14:39:13 -0700

    committing files

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include "main.h"
+
+/**
+ * main - prints numbers(1-100) but for multiples(3,5)prints FizzBuzz
+ * @void: argument/input for this function
+ * Return: 0 - when success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	_putchar('\n');
+}
+
