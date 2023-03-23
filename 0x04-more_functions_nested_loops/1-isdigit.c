e0233fcca49bc78320433966d47fec19ce207996
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-23 10:52:42 -0700

    committing files

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,18 +8,13 @@
  */
 int _isdigit(int c)
 {
-	int a[11] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int i;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		if (a[i] == c)
+		if (i == c)
 		{
-			_putchar('1');
-		}
-		else
-		{
-			_putchar('0');
+			return (1);
 		}
 	}
 	return (0);
