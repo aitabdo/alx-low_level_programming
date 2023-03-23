c604bb6827955a8e6992a6c104e9ee3c9aaf46f8
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-22 17:25:36 -0700

    committing files

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,16 +2,16 @@
 
 /**
  * main - prints sum of all multiples of 3 or 5
- * @void: no arguments for main
  * for loop loops over numbers bellow 1024
- * Return :0
+ * Return: 0 - when success
  */
-int main()
+int main(void)
 {
 	int i;
-	int summ=0;
-	int summ1=0;
-	for(i=0 ;i<1024;i++)
+	int summ = 0;
+	int summ1 = 0;
+
+	for (i = 0 ; i < 1024 ; i++)
 	{
 		if (i % 3 == 0)
 		{
@@ -22,5 +22,6 @@ int main()
 			summ1 = summ1 + i;
 		}
 	}
-	return (summ + summ1);
+	printf("%d", summ + summ1);
+	return (0);
 }
