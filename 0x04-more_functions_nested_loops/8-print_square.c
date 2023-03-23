56800b4dc3937e7fa64c819bad1838f533c2ee10
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-23 14:08:00 -0700

    committing files

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - prints a square, followed by a new line
+ * @size: argument/input for this function
+ * Return: 0 - when success
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size < 0 || size == 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (i = 0 ; i < size ; i++)
+		{
+			for (j = 0 ; j < size ; j++)
+			{
+				_putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+}
