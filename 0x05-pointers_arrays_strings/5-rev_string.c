2ad5688641a58f41fa605c7410964a4651bd2343
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-03-27 19:56:13 -0700

    committing files

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,16 +9,22 @@
  */
 void rev_string(char *s)
 {
-	int i;
-	int j = strlen(s);
-	int tmp;
+	int i, len, len2;
+	char temp;
 
-	j = j - 1;
-	for (i = 0; i <= j / 2; i++)
+	len = 0;
+	len2 = 0;
+	while (s[len] != '\0')
 	{
-		tmp = s[i];
-		s[i] = s[j - i];
-		s[j] = tmp;
-		j--;
+		len++;
+	}
+
+	len2 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len2];
+		s[len2--] = temp;
 	}
 }
