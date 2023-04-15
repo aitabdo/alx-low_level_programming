f143ff9992f46bd5cb861135cb255f2f38a38513
aitabdo <125477928+aitabdo@users.noreply.github.com>
2023-04-15 23:38:05 +0000

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* By Abdelghni Hamanar */
+/**
+* main - Entry point
+* @argc: variable
+* @argv: variable pointer
+* Return: Always 0 (success)
+*/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	/* we will use printf("%s\n", *argv);  to print string*/
+	printf("%s\n", argv[0]);
+	return (0);
+}
