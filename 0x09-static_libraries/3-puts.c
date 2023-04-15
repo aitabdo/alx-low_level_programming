85b2c87c3b852d77976827204d471c39a827fdc5
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-04-15 16:05:42 -0700

    committing files

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,19 +1,13 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
- * _puts - prints a string to stdout
- * @str: argument/input for this function
- * Return: 0 - when success
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
  */
 void _puts(char *str)
 {
-	int i = 0;
-
-	while (str[i] != '\0')
+	while (*str != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(*str++);
 	}
-	_putchar('\n');
+		_putchar('\n');
 }
