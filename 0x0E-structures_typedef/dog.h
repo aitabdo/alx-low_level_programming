45f64aa90462e948aa26608346f3b752f11f4e2e
aitabdo <125477928+aitabdo@users.noreply.github.com>
2023-04-18 22:42:31 +0000

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog into
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner dog name
+ *
+ * Description: description for my struct goes here !
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for my struct
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
