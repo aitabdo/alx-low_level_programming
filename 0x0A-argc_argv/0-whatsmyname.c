aabadb971d39fd17fbc114e8ec78e0546b3a1a90
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-06-14 05:57:22 -0700

    committing files

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,8 @@
  * Return: (Success)
  */
 
-int main(__attribute__ ((unused)) int argc,char *argv[])
+int main(__attribute__ ((unused)) int argc, char *argv[])
 {
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
