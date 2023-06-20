3d0efd5a4e82ffeaa58c7a03ba984ca828c62672
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-06-20 13:49:03 -0700

    committing files

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,15 @@
-typedef struct dog
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog into
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Description: struct description
+ */
+
+struct dog
 {
 	char *name;
 	float age;
