8f224522b2ffc95eedd883f9e359be7f3234f2ec
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-10-03 17:51:03 -0700

    committing changes

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include<stdio.h>
+
+int _is_palindrome(char *s, int i);
+
+/**
+ * is_palindrome - a function that returns 1 if *s is a palindrome and 0 if not
+ * @s: argument for this function
+ * Return: (Success)
+ */
+
+int is_palindrome(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (_is_palindrome(s, 1));
+}
+
+/**
+ * _is_palindrome - a function that calculates if *s is palindrome or not.
+ * @s: argument for this function
+ * @i: argument for this function
+ * Return: (Success)
+ */
+
+int _is_palindrome(char *s, int i)
+{
+	if (*s != s[strlen(s) - i])
+	{
+		return (0);
+	}
+
+	if (i == (strlen(s) / 2))
+	{
+		return (1);
+	}
+	s++;
+	return (_is_palindrome(s, i + 1));
+
+}
