6fc41fd317a85a3cccf73836a78bc0a7b9555990
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-10-05 18:05:48 -0700

    committing changes

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * *str_concat - a function that concat s1 and s2
+ * @s1: arguments for this function
+ * @s2: arguments for this function
+ * Return: (Success)
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int len_s1 = 0;
+	unsigned int len_s2 = 0;
+	unsigned int total_size = len_s1 + len_s2;
+	char *t;
+
+	if (!(s1 && s2))
+	{
+		return (NULL);
+	}
+
+	while (s1[len_s1])
+	{
+		len_s1++;
+	}
+	while (s2[len_s2])
+	{
+		len_s2++;
+	}
+	t = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
+	if (!t)
+	{
+		return (NULL);
+	}
+	while (i < len_s1)
+	{
+		t[i] = s1[i];
+		i++;
+	}
+	while (j < len_s2)
+	{
+		t[len_s1] = s2[j];
+		len_s1++;
+		j++;
+	}
+
+	return (t);
+}
