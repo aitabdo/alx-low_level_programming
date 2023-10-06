88b88fbdc7821854702365a9df51bfbb30049eab
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-10-05 19:46:46 -0700

    committing changes

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,14 +19,14 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	grid = malloc(sizeof(int *) * height);
+	grid = (int **)malloc(sizeof(int *) * height);
 	if (!grid)
 	{
 		return (NULL);
 	}
 	for (i = 0 ; i < width ; i++)
 	{
-		grid[i] = malloc(sizeof(int) * width);
+		grid[i] = (int *)malloc(sizeof(int) * width);
 		if (!grid[i])
 		{
 			free(grid);
