9239539423b4f771993e1bb84eed83e4446c0496
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-10-06 15:41:31 -0700

    committing files

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *@argc: number of arguments given when running this program.
+ *@argv: list of arguments given.
+ * Return: Always 0 (Success)
+ **/
+
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
