dd6fd673d6d9923cdbffa085e94c064541b52185
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-10-06 15:38:59 -0700

    committing files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *@argc: number of arguments given when running this program.
+ *@argv: list of arguments given.
+ * Return: Always 0 (Success)
+ **/
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
