ca1b9a0f015f7efabd69b8cd4666358524420ab1
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-10-07 15:50:34 -0700

    committing files

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,26 +14,18 @@ int main(int argc, char *argv[])
 	int j;
 	int sum = 0;
 
-	if (argc <= 2)
+	for (i = 1 ; i < argc ; i++)
 	{
-		printf("0");
-		printf("\n");
-	}
-	else
-	{
-		for (i = 1 ; i < argc ; i++)
+		for (j = 0 ; argv[i][j] ; j++)
 		{
-			for (j = 0 ; argv[i][j] ; j++)
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
-				if (argv[i][j] < '0' || argv[i][j] > '9')
-				{
-					printf("Error\n");
-					return (1);
-				}
-				sum = sum + atoi(argv[i]);
+				printf("Error\n");
+				return (1);
 			}
 		}
-		printf("%d\n", sum);
+		sum = sum + atoi(argv[i]);
 	}
+	printf("%d\n", sum);
 	return (0);
 }
