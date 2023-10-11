f8d93a1a642ada11ca226545cee9bf2a1cd6ea9e
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-10-10 19:02:23 -0700

    committing files

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -21,5 +21,5 @@ void print_dog(struct dog *d)
 	{
 		d->owner = "nil";
 	}
-	printf("Name: %s\nAge: %f\nOwner: %s", d->name, d->age, d->owner)
+	printf("Name: %s\nAge: %f\nOwner: %s", d->name, d->age, d->owner);
 }
