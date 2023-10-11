91f2b72348e6ea3243252d4089e980b167beebf0
aitabdo <125477928+aitabdo@users.noreply.github.com>
2023-10-11 03:16:31 +0100

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,16 +12,13 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	{
 		return;
-	}
+	
 	if (d->name == NULL)
-	{
 		d->name = "(nil)";
-	}
+	
 	if (d->owner == NULL)
-	{
 		d->owner = "(nil)";
-	}
+	
 	printf("Name: %s\nAge: %f\nOwner: %s", d->name, d->age, d->owner);
 }
