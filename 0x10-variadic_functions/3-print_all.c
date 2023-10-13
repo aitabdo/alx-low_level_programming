8680a5d6d1499dc0a3fd6d9a54ebab7fc48b6749
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-10-13 08:12:42 -0700

    committing files

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - a function that prints anything.
+ * @format: format to print
+ * Return: Nothing.
+ */
+
+void print_all(const char * const format, ...)
+{
+	char *s;
+	int i = 0;
+	char *separator = "";
+	va_list args;
+
+	va_start(args, format);
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'i':
+					printf("%s%d", separator, va_arg(args, int));
+					break;
+				case 'f':
+					printf("%s%f", separator, va_arg(args, float));
+					break;
+				case 'c':
+					printf("%s%c", separator, va_arg(args, char));
+					break;
+				case 's':
+					s = va_arg(args, char*);
+					if (!s)
+					{
+						s = "(nil)";
+					}
+					printf("%s%s", separator, s);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			separator = ', ';
+			i++;
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
