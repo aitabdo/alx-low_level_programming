a17484752ec04ce3b386aa35b2c3061bb0bbca9f
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-10-13 17:19:14 -0700

    committing files

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,8 +22,18 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	f = get_op_func(argv[2]);
+	if (!f)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
+	if ((argv[2] == '/' || argv[2] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	result = f(a, b);
 	printf("%d\n", result);
 	return (0);
