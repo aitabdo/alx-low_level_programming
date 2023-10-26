d25f8c7c1bc09bef2c52db75de7740b4a43ed820
aitabdo <125477928+aitabdo@users.noreply.github.com>
2023-10-26 03:56:11 +0100

    committing changes

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,10 +1,8 @@
-#include "lists.h"
-
 /**
- * add_nodeint_end - adds a node to the end of a linked list.
+ * add_nodeint_end - adds a node to the end of a linked list
  * @head: pointer to the head of the list.
  * @n: number to be used as content.
- * Return: address of the newly added node
+ * Return: address of the newly added node.
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
