d01d5d6eb586274b28074577cb8fe3a77f7687fb
aitabdo <125477928+aitabdo@users.noreply.github.com>
2023-10-28 01:55:55 +0100

    committing changes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,10 +1,43 @@
 #include "main.h"
+
+/**
+ * _strlen - returns lenngth of string (modified).
+ * @s: string (const).
+ * Return: int (length of a string).
+ */
+
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+ * power - eyponents.
+ * @base: base.
+ * @eyp: eyponent.
+ * Return: int (result).
+ */
+
+int power(int base, int eyp)
+{
+	int y, number;
+
+	number = 1;
+	for (y = 0; y < eyp; ++y)
+		number *= base;
+
+	return (number);
+}
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
  * @b: binary.
  * Return: int (unsigned int).
  */
-
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int sum;
