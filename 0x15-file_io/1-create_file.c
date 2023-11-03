9b7802a06e9cd12021858ff70826cebbe9a9d6e9
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-11-03 16:53:31 -0700

    committing files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,10 +18,9 @@ int create_file(const char *filename, char *text_content)
 	{
 		text_content = "";
 	}
-	while (*text_content)
+	while (text_content[length] != '\0')
 	{
 		length++;
-		text_content++;
 	}
 	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
