a484602050f97e84c5e4e68d44f215cf01de4da0
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-11-03 16:47:06 -0700

    committing files

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends a text at the end of a fp.
+ * @filename: name of file.
+ * @text_content: content of our file.
+ * Return: 1 - success, -1 error
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int length = 0, fp;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+	while (*text_content)
+	{
+		length++;
+		text_content++;
+	}
+	fp = open(filename, O_WRONLY | O_APPEND);
+
+	if (fp == -1)
+	{
+		return (-1);
+	}
+
+	write(fp, text_content, length);
+	return (1);
+}
