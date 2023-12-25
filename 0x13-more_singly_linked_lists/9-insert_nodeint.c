d022c60a0ba70addcf67841c6535259ce9d52761
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-12-25 11:07:32 -0800

    committing files

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,33 +10,36 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *tmp = *head;
+	listint_t *len_of_linked_list = *head;
 	listint_t *new_node;
 	unsigned int i = 0;
+	unsigned int len_of_list = 0;
 
+	while (len_of_linked_list != NULL)
+	{
+		len_of_linked_list = len_of_linked_list->next;
+		len_of_list++;
+	}
+	if (idx > len_of_list)
+		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 		return (NULL);
-	new_node->n = n;
-
 	if (*head == NULL)
-	{
 		return (NULL);
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
 	while (i < idx - 1)
 	{
-		if (tmp)
-			tmp = tmp->next;
-		else
-		{
-			return (NULL);
-		}
+		tmp = tmp->next;
 		i++;
 	}
-	if (tmp->next)
-	{
-		new_node->next = tmp->next;
-		tmp->next = new_node;
-		return (new_node);
-	}
-	return (NULL);
+	new_node->next = tmp->next;
+	tmp->next = new_node;
+	return (new_node);
 }
