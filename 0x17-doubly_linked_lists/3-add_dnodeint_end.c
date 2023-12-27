6598d0d5f5014193c21a709d2cfb392eab122ed3
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-12-27 11:37:32 -0800

    committing files

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -13,6 +13,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		dlistint_t *new_node;
 
 		new_node = malloc(sizeof(dlistint_t));
+		if (!new_node)
+			return (NULL);
 		new_node->n = n;
 		new_node->next = NULL;
 		new_node->prev = NULL;
