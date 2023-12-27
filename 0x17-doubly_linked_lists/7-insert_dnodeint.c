960be1ddc1a48add9aa7d81e8749f29fd43a8d46
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-12-27 12:23:02 -0800

    committing files

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,12 +12,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node;
 	unsigned int i = 0;
 
+	if (h == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
 
-	if (h == NULL)
-		return (NULL);
 	new_node->n = n;
 	new_node->next = NULL;
 	if (idx == 0)
