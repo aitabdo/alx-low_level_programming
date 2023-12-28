8b6a685f57c7d312139a087f34cf5223d30b3deb
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-12-28 09:45:52 -0800

    committing files

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -21,7 +21,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 				free(tmp);
 				return (1);
 			}
-			free(tmp);
+			free(tmp), tmp = NULL;
 			*head = NULL;
 			return (1);
 		}
