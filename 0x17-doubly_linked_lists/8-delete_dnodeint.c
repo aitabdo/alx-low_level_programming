6e060d7f8f86cf0477cc728d277a4a6e5826a2fa
aitabdo <abderrahmanaitsalh2001@gmail.com>
2023-12-28 09:33:55 -0800

    committing files

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - deletes a node at index.
+ * @head: first node in the linked list.
+ * @index: index to delete at.
+ * Return: int (1 if success 0 if fail);
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp = *head;
+	unsigned int i = 0;
+
+	if (index == 0)
+	{
+		if (*head)
+		{
+			if (tmp->next)
+			{
+				*head = tmp->next;
+				(*head)->prev = NULL;
+				free(tmp);
+				return (1);
+			}
+			free(tmp);
+			*head = NULL;
+			return (1);
+		}
+		return (-1);
+	}
+	else
+	{
+		while (i < index && tmp)
+		{
+			if (i == index - 1)
+			{
+				dlistint_t *tmp1 = tmp->next;
+
+				tmp->next = tmp1->next;
+				tmp1->next->prev = tmp;
+				free(tmp1);
+				return (1);
+			}
+			else
+				tmp = tmp->next;
+			i++;
+		}
+	}
+	return (-1);
+}
